094ca1c84d1d1993d10c795737074d4d5f605bbf
Anas <anas.zulkifli2001@gmail.com>
2025-05-25 22:27:47 +0800

    feat: create auth dto

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAuthDto } from './create-auth.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {
+  email?: string | undefined;
+  password?: string | undefined;
+}
